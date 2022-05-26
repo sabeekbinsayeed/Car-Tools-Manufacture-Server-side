fc12262b8ca143791c8ceb5b49cc66759601ae53
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-05-26 21:02:16 +0600

    final

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run() {
             }
             else {
                 res.status(403).send({ message: 'forbidden' });
-                console.log('admin e error hosce solve')
+                console.log('admin e error hosce solved')
             }
         }
         app.get('/tools', async (req, res) => {
